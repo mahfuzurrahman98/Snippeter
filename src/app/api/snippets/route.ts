4a72fd316ad1e97a0315d43e1d6efd0382df4947
mahfuzurrahman98 <mdmahfuzurrahmanarif@gmail.com>
2023-07-03 01:28:28 +0600

    snippet created and react-toast added

--- src/app/api/snippets/route.ts
+++ src/app/api/snippets/route.ts
@@ -1,15 +1,57 @@
 import Database from '@configs/db';
 import Snippet from '@models/Snippet';
 import { NextRequest, NextResponse } from 'next/server';
+import { v4 } from 'uuid';
+
 const db = Database.getInstance();
 
-export async function POST(req: NextRequest) {
-  // console.log(process.env.MONGO_URI);
-  const reqBody = await req.json();
-  const snippet = new Snippet(reqBody);
-  await snippet.save();
-  return NextResponse.json({
-    message: 'Snippet created successfully',
-    success: true,
-  });
-}
+export const POST = async (req: NextRequest) => {
+  try {
+    const reqBody = await req.json();
+    delete reqBody._tags;
+    console.log(reqBody);
+    // return;
+    reqBody.uuid = v4();
+    const snippet = new Snippet(reqBody);
+    await snippet.save();
+
+    return NextResponse.json({
+      message: 'Snippet created successfully',
+      success: true,
+    });
+  } catch (error: any) {
+    return NextResponse.json(
+      {
+        message: 'Failed to create snippet',
+        success: false,
+        error: error.message,
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+};
+
+export const GET = async (req: NextRequest) => {
+  try {
+    const snippets = await Snippet.find();
+
+    return NextResponse.json({
+      message: 'Snippets fetched successfully',
+      success: true,
+      snippets,
+    });
+  } catch (error: any) {
+    return NextResponse.json(
+      {
+        message: 'Failed to fetch snippets',
+        success: false,
+        error: error.message,
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+};
--- src/app/snippets/create/page.tsx
+++ src/app/snippets/create/page.tsx
@@ -1,29 +1,28 @@
 'use client';
-
 import languages from '@configs/languages';
 import axios from 'axios';
 import { ChangeEvent, FormEvent, useState } from 'react';
+import toast, { Toaster } from 'react-hot-toast';
+
+interface FormData {
+  title: string;
+  language: string;
+  sourceCode: string;
+  owner: string;
+  _tags: string;
+  tags: string[];
+}
 
 const CreateSnippet = () => {
-  const expirationOptions = Array.from({ length: 18 }, (_, index) => ({
-    value: `${index + 1}`,
-    label: `${index + 1} day${index !== 0 ? 's' : ''}`,
-  }));
-
-  const themes = [
-    { name: 'Default', alias: 'default' },
-    { name: 'Dark', alias: 'dark' },
-    { name: 'Cobalt', alias: 'cobalt' },
-  ];
-
-  const [formData, setFormData] = useState({
+  const [formData, setFormData] = useState<FormData>({
     title: '',
     language: '',
-    expiry: '',
-    theme: '',
     sourceCode: '',
     owner: '',
+    _tags: '',
+    tags: [],
   });
+  const [buttonDisabled, setButtonDisabled] = useState<boolean>(false);
 
   const handleChange = (
     event: ChangeEvent<
@@ -38,15 +37,46 @@ const CreateSnippet = () => {
 
   const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+    setButtonDisabled(true);
 
-    console.log(formData);
+    let tagsArray = formData._tags.split(',').map((tag) => tag.trim());
+    tagsArray = tagsArray.filter((tag) => tag !== '');
 
-    const response = await axios.post('/api/snippets', formData);
+    setFormData((prevData) => ({
+      ...prevData,
+      tags: tagsArray,
+    }));
+
+    try {
+      const response = await axios.post('/api/snippets', formData);
+      console.log(response.data.message);
+      toast.success(response.data.message);
+    } catch (error) {
+      console.log(error);
+      toast.error('Something went wrong!');
+    } finally {
+      setButtonDisabled(false);
+    }
   };
 
   return (
-    <div className="container mx-auto py-10 px-3">
+    <div className="container mx-auto py-10 px-5 max-w-[750px]">
       <h1 className="text-3xl font-bold mb-6">Create a Snippet</h1>
+      <Toaster
+        position="bottom-right"
+        toastOptions={{
+          className: '',
+          duration: 5000,
+          style: {
+            background: '#363636',
+            color: '#fff',
+          },
+          success: {
+            duration: 3000,
+          },
+        }}
+      />
+
       <form onSubmit={handleSubmit}>
         <div className="mb-6">
           <label htmlFor="title" className="block mb-1 font-semibold">
@@ -55,9 +85,11 @@ const CreateSnippet = () => {
           <input
             type="text"
             name="title"
+            id="title"
+            placeholder="Enter title"
             value={formData.title}
             onChange={handleChange}
-            className="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:border-indigo-500"
+            className="w-full px-3 py-2 border-2 border-gray-300 rounded focus:outline-none focus:border-black"
             required
           />
         </div>
@@ -67,12 +99,15 @@ const CreateSnippet = () => {
           </label>
           <select
             name="language"
+            id="language"
             value={formData.language}
             onChange={handleChange}
-            className="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:border-indigo-500"
+            className="w-full px-3 py-2 border-2 border-gray-300 rounded focus:outline-none focus:border-black"
             required
           >
-            <option value="">Select a language</option>
+            <option value="" selected>
+              Select a language
+            </option>
             {languages.map((lang, index) => (
               <option key={index} value={lang.ext}>
                 {lang.name}
@@ -82,54 +117,17 @@ const CreateSnippet = () => {
         </div>
 
         <div className="mb-6">
-          <label htmlFor="theme" className="block mb-1 font-semibold">
-            Theme
-          </label>
-          <select
-            name="theme"
-            value={formData.theme}
-            onChange={handleChange}
-            className="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:border-indigo-500"
-            required
-          >
-            <option value="">Select a theme</option>
-            {themes.map((theme, index) => (
-              <option key={index} value={theme.alias}>
-                {theme.name}
-              </option>
-            ))}
-          </select>
-        </div>
-
-        <div className="mb-6">
-          <label htmlFor="expiry" className="block mb-1 font-semibold">
-            Expiry
-          </label>
-          <select
-            name="expiry"
-            value={formData.expiry}
-            onChange={handleChange}
-            className="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:border-indigo-500"
-            required
-          >
-            <option value="">Select an expiry</option>
-            {expirationOptions.map((option, index) => (
-              <option key={index} value={option.value}>
-                {option.label}
-              </option>
-            ))}
-          </select>
-        </div>
-        <div className="mb-6">
           <label htmlFor="owner" className="block mb-1 font-semibold">
             Owner Name
           </label>
           <input
             type="text"
             name="owner"
+            id="owner"
+            placeholder="Enter your name"
             value={formData.owner}
             onChange={handleChange}
-            className="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:border-indigo-500"
+            className="w-full px-3 py-2 border-2 border-gray-300 rounded focus:outline-none focus:border-black"
             required
           />
         </div>
@@ -139,16 +137,34 @@ const CreateSnippet = () => {
           </label>
           <textarea
             name="sourceCode"
+            id="sourceCode"
+            placeholder="Enter source code"
             value={formData.sourceCode}
             onChange={handleChange}
-            className="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:border-indigo-500"
+            className="w-full px-3 py-2 border-2 border-gray-300 rounded focus:outline-none focus:border-black"
             rows={8}
             required
           ></textarea>
         </div>
+        <div className="mb-6">
+          <label htmlFor="tags" className="block mb-1 font-semibold">
+            Tags (comma-separated)
+            <span className="ml-1 font-normal text-gray-500">optional</span>
+          </label>
+          <input
+            type="text"
+            name="_tags"
+            id="_tags"
+            placeholder="ex: javascript, react, nodejs"
+            value={formData._tags}
+            onChange={handleChange}
+            className="w-full px-3 py-2 border-2 border-gray-300 rounded focus:outline-none focus:border-black"
+          />
+        </div>
         <button
           type="submit"
-          className="px-4 py-2 text-white bg-indigo-500 rounded hover:bg-indigo-600"
+          className="px-4 py-2 text-white bg-black rounded hover:bg-gray-700"
+          disabled={buttonDisabled}
         >
           Create Snippet
         </button>
