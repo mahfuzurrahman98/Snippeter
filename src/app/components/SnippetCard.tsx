03834907d036dfe6e0fd0bae96e703bf3a59de3b
mahfuzurrahman98 <mdmahfuzurrahmanarif@gmail.com>
2023-07-09 17:13:32 +0600

    Link instead of <a> for navigation

--- src/app/components/SnippetCard.tsx
+++ src/app/components/SnippetCard.tsx
@@ -1,21 +1,23 @@
 import { SnippetType } from '@utils/types';
+import Link from 'next/link';
 
-const SnippetCard = ({ snippet }: SnippetType) => {
+const SnippetCard = ({ snippet }: { snippet: SnippetType }) => {
   return (
     <div className="border border-gray-300 p-4 rounded-md mb-4">
-      <a href={'/snippet/' + snippet.uuid} className="text-xl font-bold">
+      <Link href={'/snippets/' + snippet.uuid} className="text-xl font-bold">
         {snippet.title}
-      </a>
+      </Link>
       <p className="text-gray-500">Language: {snippet.language}</p>
       <div className="flex mt-2">
-        {snippet.tags.map((tag: string) => (
-          <span
-            key={tag}
-            className="inline-block bg-gray-200 text-gray-700 px-2 py-1 rounded-full text-sm mr-2"
-          >
-            {tag}
-          </span>
-        ))}
+        {snippet.tags &&
+          snippet.tags.map((tag: string) => (
+            <span
+              key={tag}
+              className="inline-block bg-gray-200 text-gray-700 px-2 py-1 rounded-full text-sm mr-2"
+            >
+              {tag}
+            </span>
+          ))}
       </div>
     </div>
   );
