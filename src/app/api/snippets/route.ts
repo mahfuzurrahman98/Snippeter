9676ad8f0e73b7c936b6ac1e89497e8ed3e43663
mahfuzurrahman98 <mdmahfuzurrahmanarif@gmail.com>
2023-07-09 17:12:45 +0600

    for data fetching using lib/data instead of API

--- src/app/api/snippets/route.ts
+++ src/app/api/snippets/route.ts
@@ -31,33 +31,3 @@ export const POST = async (req: NextRequest) => {
     );
   }
 };
-
-export const GET = async (req: NextRequest) => {
-  try {
-    const page: number = Number(req.nextUrl.searchParams.get('page')) || 1;
-    const limit: number = Number(req.nextUrl.searchParams.get('limit')) || 10;
-    console.log(page, limit);
-    const snippets = await Snippet.find()
-      .sort({ createdAt: -1 })
-      .skip((page - 1) * limit)
-      .limit(limit);
-
-    return NextResponse.json(
-      {
-        message: 'Snippets fetched successfully',
-        success: true,
-        snippets,
-      },
-      { status: 200 }
-    );
-  } catch (error: any) {
-    return NextResponse.json(
-      {
-        message: 'Failed to fetch snippets',
-        success: false,
-        error: error.message,
-      },
-      { status: 500 }
-    );
-  }
-};
