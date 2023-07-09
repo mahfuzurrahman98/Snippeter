4bafbcd2f70698f80cfeedf04fc3e9546a9e2443
mahfuzurrahman98 <mdmahfuzurrahmanarif@gmail.com>
2023-07-09 17:12:14 +0600

    converted to server component

--- src/app/snippets/[snippetId]/page.tsx
+++ src/app/snippets/[snippetId]/page.tsx
@@ -1,56 +1,41 @@
-'use client';
-
 import languages from '@configs/languages';
-import axios from 'axios';
-import { useEffect, useState } from 'react';
-import toast, { Toaster } from 'react-hot-toast';
-
-type Snippet = {
-  title: string;
-  sourceCode: string;
-  language: string;
-  tags: string[];
-  uuid: string;
-  owner: string;
-  createdAt: string;
-};
+import { getSnippet } from '@lib/data';
+import { SnippetType } from '@utils/types';
+import { Toaster } from 'react-hot-toast';
 
 type Params = {
   params: {
-    snippetId: number;
+    snippetId: string;
   };
 };
 
-const Snippet = ({ params }: Params) => {
-  const [snippet, setSnippet] = useState<Snippet | null>(null);
+type Response = {
+  success: boolean;
+  message: string;
+  data?: { snippet: SnippetType } | undefined;
+};
 
-  useEffect(() => {
-    const fetchSnippet = async () => {
-      try {
-        const response = await axios.get(`/api/snippets/${params.snippetId}`);
-        setSnippet(response.data.snippet);
-      } catch (error) {
-        console.error('Error fetching snippet:', error);
-      }
-    };
-    fetchSnippet();
-  }, [params.snippetId]);
+const Snippet = async ({ params }: Params) => {
+  const response: Response = await getSnippet(params.snippetId);
+  const snippet = response.data?.snippet;
 
   if (!snippet) {
     return (
-      <div className="flex items-center justify-center h-screen">
-        Loading...
+      <div className="flex justify-center items-center h-screen bg-black text-green-400 text-2xl">
+        {'</> Snippet not found'}
       </div>
     );
   }
 
-  const formattedDate = new Date(snippet.createdAt).toLocaleString('en-US', {
-    month: 'long',
-    day: 'numeric',
-    year: 'numeric',
-    hour: 'numeric',
-    minute: 'numeric',
-  });
+  const formattedDate = snippet.createdAt
+    ? new Date(snippet.createdAt).toLocaleString('en-US', {
+        month: 'long',
+        day: 'numeric',
+        year: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric',
+      })
+    : 'Unknown Date';
 
   return (
     <div className="max-w-3xl mx-auto py-7 px-5">
@@ -70,23 +55,13 @@ const Snippet = ({ params }: Params) => {
       />
       <h1 className="text-3xl font-bold mb-4">{snippet.title}</h1>
       <p className="text-gray-500 mb-2">Created at: {formattedDate}</p>
-      <p className="text-gray-500 mb-4">Tags: {snippet.tags.join(', ')}</p>
+      <p className="text-gray-500 mb-4">Tags: {snippet.tags?.join(', ')}</p>
       <p className="text-gray-500 mb-4">Owner: {snippet.owner}</p>
 
       <div className="flex justify-between items-center bg-gray-700 py-1 px-3 rounded-t-md">
         <span className="text-white">
           {languages.find((lang) => lang.ext === snippet.language)?.name}
         </span>
-        <button
-          className="font-semibold rounded-md p-1 bg-white flex gap-x-1 items-center"
-          onClick={() => {
-            navigator.clipboard.writeText(snippet.sourceCode);
-            toast.success('Copied to clipboard!');
-          }}
-        >
-          <img src="/copy.svg" alt="copy" width={12} className="text-white" />
-          <span className="text-black text-xs">Copy</span>
-        </button>
       </div>
       <pre className="bg-black rounded-b-md p-4 max-w-full overflow-x-auto">
         <code
