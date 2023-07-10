df75ab16bb183b4f55c18eaeb0b8e39dab29a10c
mahfuzurrahman98 <mdmahfuzurrahmanarif@gmail.com>
2023-07-11 01:44:29 +0600

    create button link added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import { getAllSnippets } from '@/lib/data/snippets';
 import { SnippetType, searchParamsType } from '@utils/types';
+import Link from 'next/link';
+import Search from './components/Search';
 import SnippetCard from './components/SnippetCard';
 
 const Snippets = async ({
@@ -19,8 +21,18 @@ const Snippets = async ({
   const snippets = response.data?.snippets || [];
 
   return (
-    <div className=" p-5">
-      <h1 className="text-3xl font-bold mb-5">Snippets</h1>
+    <div className="p-5 max-w-screen-xl mx-auto">
+      <div className="flex justify-between items-start mb-5 border-b-4 border-gray-700">
+        <h1 className="text-2xl md:text-3xl font-bold mb-6">Snippets</h1>
+        <Link
+          className="px-3 py-1 text-white rounded bg-black hover:bg-gray-600"
+          href="/create"
+        >
+          Create
+        </Link>
+      </div>
+
+      <Search />
 
       {snippets.map((snippet: SnippetType) => (
         <SnippetCard key={snippet.uuid} snippet={snippet} />
