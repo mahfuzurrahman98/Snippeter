2b9d39ce59e592946412f7c369c58cbb5d1035d6
mahfuzurrahman98 <mdmahfuzurrahmanarif@gmail.com>
2023-07-11 18:42:50 +0600

    A home page navigation added

--- src/app/[snippetId]/page.tsx
+++ src/app/[snippetId]/page.tsx
@@ -2,6 +2,7 @@ import CopyButton from '@app/components/CopyButton';
 import languages from '@lib/data/languages';
 import { getSnippet } from '@lib/data/snippets';
 import { SnippetType } from '@utils/types';
+import Link from 'next/link';
 
 type Params = {
   params: {
@@ -39,10 +40,20 @@ const Snippet = async ({ params }: Params) => {
 
   return (
     <div className="max-w-3xl mx-auto py-7 px-5">
-      <h1 className="text-3xl font-bold mb-4">{snippet.title}</h1>
-      <p className="text-gray-700 mb-2">Created at: {formattedDate}</p>
-      <p className="text-gray-700 mb-4">Tags: {snippet.tags?.join(', ')}</p>
-      <p className="text-gray-700 mb-4">Owner: {snippet.owner}</p>
+      <div className="flex justify-between items-start">
+        <div>
+          <h1 className="text-3xl font-bold mb-4">{snippet.title}</h1>
+          <p className="text-gray-700 mb-2">Created at: {formattedDate}</p>
+          <p className="text-gray-700 mb-4">Tags: {snippet.tags?.join(', ')}</p>
+          <p className="text-gray-700 mb-4">Owner: {snippet.owner}</p>
+        </div>
+        <Link
+          className="px-3 py-1 text-white rounded bg-black hover:bg-gray-600"
+          href="/"
+        >
+          Home
+        </Link>
+      </div>
 
       <div className="flex justify-between items-center bg-gray-700 py-1 px-3 rounded-t-md">
         <span className="text-white">
