dcb030b3c859295693075ef8f90fc7b92aa7d923
mahfuzurrahman98 <mdmahfuzurrahmanarif@gmail.com>
2023-07-15 01:17:23 +0600

    metadata updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,14 @@ import './globals.css';
 export const metadata = {
   title: 'Snippeter',
   description: 'view all snippets',
+  openGraph: {
+    type: 'website',
+    locale: 'en_IE',
+    url: 'https://snippeter.vercel.app/',
+    title: 'Snippeter',
+    description: 'view snippets',
+    image: '/og.png',
+  },
 };
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
