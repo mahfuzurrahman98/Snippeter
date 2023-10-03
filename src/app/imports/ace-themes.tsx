d73dc90e55aaaabde3302a80c9ad853b47cd31bc
mahfuzurrahman98 <mdmahfuzurrahmanarif@gmail.com>
2023-10-03 11:34:28 +0600

    global imports for ace themes and languages

---
+++ src/app/imports/ace-themes.tsx
@@ -0,0 +1,10 @@
+import 'ace-builds/src-noconflict/theme-chrome';
+import 'ace-builds/src-noconflict/theme-cobalt';
+import 'ace-builds/src-noconflict/theme-dracula';
+import 'ace-builds/src-noconflict/theme-github';
+import 'ace-builds/src-noconflict/theme-github_dark';
+import 'ace-builds/src-noconflict/theme-gob';
+import 'ace-builds/src-noconflict/theme-monokai';
+import 'ace-builds/src-noconflict/theme-solarized_light';
+import 'ace-builds/src-noconflict/theme-xcode';
+
